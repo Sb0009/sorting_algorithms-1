b7a7dbbb0c810562a4b98adb154d2ef0bf75cb7a
DevMust <elmustapha.abourar@outlook.com>
2022-06-13 11:45:25 +0200

    Bubble Sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,46 +1,33 @@
 #include "sort.h"
 /**
- * smallest - fucntion to get the smallest index of array
- * @array: the array to be sorted
- * @n: size of the array
- * @k: pivot
- * Return: void
- */
-int smallest(int array[], int k, int n)
-{
-	int pos = k;
-	int small = array[k];
-	int i;
-
-	for (i = k + 1; i < n; i++)
-	{
-		if (array[i] < small)
-		{
-			small = array[i];
-			pos = i;
-		}
-	}
-	return (pos);
-}
-/**
- *selection_sort - function to sort an array using selection sort method
+ *selection_sort - function to sort an array using selection sort
  *@array: the array to be sorted
  *@size: size of the array
  *Return: void
  */
 void selection_sort(int *array, size_t size)
 {
-	int k;
-	int pos;
-	int temp;
-	int n = size;
+	size_t i = 0;
+	size_t j, min, temp;
 
-	for (k = 0; k < n; k++)
+	for(i = 0; i < size - 1; i++)
 	{
-		pos = smallest(array, k, n);
-		temp = array[k];
-		array[k] = array[pos];
-		array[pos] = temp;
+		min = i;
+		for(j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+
+		}
+		if (min == i)
+		{
+			continue;
+		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
 		print_array(array, size);
 	}
 }
