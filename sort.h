bd5c72aa6648f8a0215d74405032b20b53f19932
DevMust <elmustapha.abourar@outlook.com>
2022-06-13 12:49:16 +0200

    Quick Sorting

--- sort.h
+++ sort.h
@@ -25,5 +25,9 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void print_sort(int array[], size_t size, int init);
+size_t partition(int array[], size_t size);
+void quick_sort(int *array, size_t size);
+void print_sort(int array[], size_t size, int init);
 
 #endif
