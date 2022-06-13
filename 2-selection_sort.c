8e8a8aefaa8c6e6ad6f2162d20143065637b0ef4
DevMust <elmustapha.abourar@outlook.com>
2022-06-13 11:46:15 +0200

    Bubble Sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,10 +10,10 @@ void selection_sort(int *array, size_t size)
 	size_t i = 0;
 	size_t j, min, temp;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for(j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 			{
